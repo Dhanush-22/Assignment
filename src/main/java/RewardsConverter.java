f5cc1df413e7aed983719a8d89fac9c818b0a4e5
Dhanush Bedre <bedredhanush37@gmail.com>
2024-03-23 19:47:28 +0530

    First Task

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,20 +1,53 @@
 import java.util.Scanner;
 
+
+
+class RewardValue{
+    
+    private double cashValue;
+    private double milesValue;
+
+    RewardValue(double cashValue){
+        this.cashValue = cashValue;
+    }
+
+    RewardValue(int milesValue){
+        this.milesValue = milesValue;
+    }
+
+
+    double getCashValue(){
+        return this.milesValue*0.0035;
+    }
+
+    // miles to cash at a rate of 0.0035.
+
+    double getMilesValue(){
+        return this.cashValue/0.0035;
+    }
+}
+
 public class RewardsConverter {
     public static void main(String[] args) {
+
         var scanner = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Please enter a cash value to convert to airline miles: ");
         var input_value = scanner.nextLine();
+
         double cashValue;
+        
         try {
             cashValue = Double.parseDouble(input_value);
         } catch (NumberFormatException exception) {
             System.out.println("Could not parse input value as a double, exiting");
             return;
         }
+
         System.out.println("converting $" + input_value + " to miles");
+        
         var rewardsValue = new RewardValue(cashValue);
+        
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
